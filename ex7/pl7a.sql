12eeac6d8eb3979b0b6adf2d9f198fd9e1a744aa
anisham197 <anisha.m.197@gmail.com>
2016-11-28 20:05:20 +0530

    Update pl7a.sql

--- ex7/pl7a.sql
+++ ex7/pl7a.sql
@@ -2,23 +2,24 @@ SET SERVEROUTPUT ON
 DECLARE
 	n number;
 	i number;
-	counter number;
+	flag number;
 BEGIN
         n:=&n;
         i:=1;
-        counter:=0;
+        flag:=0;
         if n=1
            then dbms_output.put_line('1 is neither prime nor composite.');
-       elsif n=2
+        elsif n=2
            then dbms_output.put_line('2 is even prime');
-       else
-           for i in 1..n loop
+        else
+           for i in 2..n/2 loop
                if mod(n,i)=0
-                   then counter:=counter+1;
+                   then flag:= 1;
+                   exit;
                end if;
            end loop;
-     end if;
-       if counter=2
+       end if;
+       if flag = 0
            then dbms_output.put_line(n||' is a prime No.');
        else
            dbms_output.put_line(n||' is a not prime No.');
